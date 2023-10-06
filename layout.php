409b7daaa80af337747f5ff7b8218c894f3c9ae8
arro38 <etienne.vaytilingom.pro@gmail.com>
2023-10-06 09:39:40 +0400

    boilerplate html added to layout

--- layout.php
+++ layout.php
@@ -1,2 +1,17 @@
 <?php
 $titre = "reseau social";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
